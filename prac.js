9bbcc1b520feb0a6478705357d923adfdbcf6ecb
parth-12797 <78936647+parth-12797@users.noreply.github.com>
2021-03-18 19:20:52 -0400

    Update prac.js

--- prac.js
+++ prac.js
@@ -35,4 +35,5 @@ $(document).ready(function() {
     $("article a:first").remove();
     $("article p:last").after(
         "<a href='#top'>Back to top</a>");
+    $("h2").css("color","green");
 })
